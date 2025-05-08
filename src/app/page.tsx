15169e390cf3dd869fb80e5eb8ed03d93e55f4c8
blesswrld <roninfortnite71@gmail.com>
2025-05-08 14:09:02 +0300

    add navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
 import { Button } from "@/components/ui/button";
-import {
-    SignedIn,
-    SignedOut,
-    SignInButton,
-    SignUpButton,
-    UserButton,
-} from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
-    return (
-        <div className="m-10">
-            <SignInButton>
-                <Button>Sign in</Button>
-            </SignInButton>
-        </div>
-    );
+    return <div className="m-10">home page</div>;
 }
