180b2705fc90a3420c91f8a4a67b5a00e0121ffc
blesswrld <roninfortnite71@gmail.com>
2025-05-09 05:02:53 +0300

    delete comms from homepage

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -10,8 +10,7 @@ import { useRouter } from "next/navigation";
 import MeetingModal from "@/components/MeetingModal";
 import LoaderUI from "@/components/LoaderUI";
 import { Loader2Icon } from "lucide-react";
-import toast from "react-hot-toast";
-// import MeetingCard from "@/components/MeetingCard";
+import MeetingCard from "@/components/MeetingCard";
 
 export default function Home() {
     const router = useRouter();
@@ -91,12 +90,12 @@ export default function Home() {
                             </div>
                         ) : interviews.length > 0 ? (
                             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                                {/* {interviews.map((interview) => (
+                                {interviews.map((interview) => (
                                     <MeetingCard
                                         key={interview._id}
                                         interview={interview}
                                     />
-                                ))} */}
+                                ))}
                             </div>
                         ) : (
                             <div className="text-center py-12 text-muted-foreground">
